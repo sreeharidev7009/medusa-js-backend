bbb7b9d30ac4b5b2baf146290388a935526b97e5
sreeharidev7009 <sreehari.7009@woxro.com>
2025-08-14 14:55:32 +0530

    updated-code

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,4 +1,27 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+  // import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+  // loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+  // module.exports = defineConfig({
+  //   projectConfig: {
+  //     databaseUrl: process.env.DATABASE_URL,
+  //     http: {
+  //       storeCors: process.env.STORE_CORS!,
+  //       adminCors: process.env.ADMIN_CORS!,
+  //       authCors: process.env.AUTH_CORS!,
+  //       jwtSecret: process.env.JWT_SECRET || "supersecret",
+  //       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+  //     }
+  //   },
+  //   modules: [
+  //     {
+  //       resolve: "./src/modules/loyalty",
+  //     },
+  //   ],
+  // })
+
+
+  import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
@@ -16,6 +39,15 @@ module.exports = defineConfig({
   modules: [
     {
       resolve: "./src/modules/loyalty",
+    },
+    {
+      resolve: "./src/modules/product-review",
+    }
+  ],
+  plugins: [
+    {
+      resolve: "medusa-plugin-wishlist",
+      options: {},
     },
   ],
 })
